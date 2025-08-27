992a2703af1e3192ae91a3d892adec8f661f2c71
Guilherme <ggk0830755@gmail.com>
2025-08-27 17:50:25 -0300

    passando para a pasta

---
+++ CrudGelt/FormData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudGelt
+{
+    public partial class FormData : Form
+    {
+        public FormData()
+        {
+            InitializeComponent();
+            Inicializar();
+        }
+
+        DataTable dt = new DataTable();
+
+        public void Inicializar()
+        {
+            dt = Livros.GetLivros();
+            DgvLivros.DataSource = dt;
+        }
+
+
+
+        private void BtnAdicionar_Click(object sender, EventArgs e)
+        {
+            using (var frm = new FormCadastro(0))
+            {
+                frm.ShowDialog();
+                DgvLivros.DataSource = Livros.GetLivros();
+            }
+        }
+
+        private void BtnAlterar_Click(object sender, EventArgs e)
+        {
+            var id = Convert.ToInt32(DgvLivros.Rows[DgvLivros.CurrentCell.RowIndex].Cells["id"].Value);
+            using (var frm = new FormCadastro(id))
+            {
+                frm.ShowDialog();
+            }
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExcluir_Click(object sender, EventArgs e)
+        {
+            var id = Convert.ToInt32(DgvLivros.Rows[DgvLivros.CurrentCell.RowIndex].Cells["id"].Value);
+            using (var frm = new FormCadastro(id, true))
+            {
+                frm.ShowDialog();
+            }
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            dt = Livros.GetLivros(TxtBuscar.Text);
+            DgvLivros.DataSource= dt;
+        }
+
+
+    }
+}
